c3780c784cfa4becaed67eae98cc7abad2aa2954
Simbayyy <simon.baillet.gomez@gmail.com>
2023-09-02 01:05:48 +0200

    feat: GENERATED to SERIAL as psql is <10

--- src/app.ts
+++ src/app.ts
@@ -42,12 +42,12 @@ let is_db_initialized = pool.query("SELECT * FROM information_schema.tables \
   WHERE table_name = 'recipe';").then(async (result) => {
       if (result.rows.length == 0) {
         await pool.query("CREATE TABLE recipe (\
-          recipe_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),\
+          recipe_id SERIAL NOT NULL PRIMARY KEY,\
           name VARCHAR(500),\
           url VARCHAR(500) UNIQUE \
           );")
         await pool.query("CREATE TABLE ingredient (\
-          ingredient_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),\
+          ingredient_id SERIAL NOT NULL PRIMARY KEY,\
           name VARCHAR(200) UNIQUE\
           );")
         await pool.query("CREATE TABLE recipe_ingredient (\
