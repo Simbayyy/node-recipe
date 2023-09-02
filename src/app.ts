88ccec392423d1428733158c113af3fcaa7261a5
Simbayyy <simon.baillet.gomez@gmail.com>
2023-09-02 17:49:52 +0200

    feat:new script to reset DB

--- src/app.ts
+++ src/app.ts
@@ -38,42 +38,6 @@ app.use(express.json())
 app.use('/', routes.router)
 app.set('view engine', hbs)
 
-  if (process.env.IN_CI != 'true'){
-    logger.log({
-      level:'info',
-      message:"Attempting check for creation of clean new tables"
-    })
-  let is_db_initialized = pool.query("SELECT * FROM information_schema.tables \
-    WHERE table_name IN ('recipe', 'ingredient', 'recipe_ingredient', 'recipe_time');").then(async (result) => {
-        if (result.rows.length != 4) {
-          await pool.query("DROP TABLE IF EXISTS recipe, ingredient, recipe_ingredient, recipe_time")
-          await pool.query("CREATE TABLE recipe (\
-            recipe_id SERIAL NOT NULL PRIMARY KEY,\
-            name VARCHAR(500),\
-            url VARCHAR(500) UNIQUE \
-            );")
-          await pool.query("CREATE TABLE ingredient (\
-            ingredient_id SERIAL NOT NULL PRIMARY KEY,\
-            name VARCHAR(200) UNIQUE\
-            );")
-          await pool.query("CREATE TABLE recipe_ingredient (\
-            recipe_id INT,\
-            ingredient_id INT,\
-            amount INT, \
-            unit VARCHAR(100),\
-            PRIMARY KEY (recipe_id, ingredient_id),\
-            CONSTRAINT fk_recipe FOREIGN KEY(recipe_id) REFERENCES recipe(recipe_id),\
-            CONSTRAINT fk_ingredient FOREIGN KEY(ingredient_id) REFERENCES ingredient(ingredient_id)\
-            );")
-          await pool.query("CREATE TABLE recipe_time (\
-            time_id SERIAL NOT NULL PRIMARY KEY,\
-            recipe_id INT,\
-            time INT, \
-            unit VARCHAR(100),\
-            CONSTRAINT fk_recipe FOREIGN KEY(recipe_id) REFERENCES recipe(recipe_id)\
-            );")        }
-      })
-}
 app.listen(port, async () => {
   logger.log({
     level: 'info',
